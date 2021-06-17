41ef9568c3bbd8e1dcb6e9218b6874dc0b1ebec7
Frank Malley <frankmalley360@gmail.com>
2021-06-17 16:53:36 +0300

    Initial commit

---
+++ pos-system/README.md
@@ -0,0 +1,2 @@
+# Point-Of-Sale-System
+A Point of Sale system written in React.js &amp; Node.js
---
+++ pos-system/react-pos/src/js/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Header.css';
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <div className="text-center">
+    <h1>
+      <a href="/#/">Point of Sale System</a>
+    </h1>
+
+    <ul className="nav-menu">
+      <li className="lead">
+        <Link to="/inventory">Inventory</Link>
+      </li>
+      <li className="lead">
+        <Link to="/">POS</Link>
+      </li>
+      <li className="lead">
+        <Link to="/transactions">Transactions</Link>
+      </li>
+      <li className="lead">
+        <Link to="/livecart">LiveCart</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Header;
+
